9d5f22886c840a1c87052ca45d376a0a801200e6
Luiza <106697295+Luizalaram@users.noreply.github.com>
2022-09-15 11:11:29 -0300

    Add files via upload

---
+++ MostrarNotas.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main(void)
+{
+    float notas[6] = {1,4,9,8,7,3};
+    
+    printf ("Mostrando notas:\n");
+    printf("notas[0] = %1f\n", notas [0]);
+    printf("notas[1] = %1f\n", notas [1]);
+    printf("notas[2] = %1f\n", notas [2]);
+    printf("notas[3] = %1f\n", notas [3]);
+    printf("notas[4] = %1f\n", notas [4]);
+    printf("notas[5] = %1f\n", notas [5]);
+
+    
+    getch();
+    return 0;
+}
